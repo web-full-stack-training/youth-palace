dc71ce088fb23b24749efd18614ac97a09819606
Artyom Margaryan <artyom570@gmail.com>
2021-12-01 00:49:22 +0400

    footer

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,106 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+</head>
+<body>
+<!--links for styles -->
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="../youth-palace/resources/css/footer-styles.css">
+
+<!--footer-side start-->
+
+<div class="container-fluid px-1 py-5 mx-auto">
+    <div class="row d-flex justify-content-center">
+        <div class="col-xl-12 col-lg-10 col-md-11 col-12 ">
+
+
+            <div class="card">
+
+                <div class="footer-left-side">
+                    <p class="contact">FOLLOW US</p>
+                    <div class="followUs d-flex ">
+
+                        <div class="links-social">
+                            <a href="https://www.facebook.com/YPCNCO" target="_blank" >
+                                <i class="fab fa-facebook-f fa-2x"></i>
+                            </a>
+                        </div>
+                        <div class="links-social">
+                            <a href="https://am.linkedin.com/in/youth-palace-579a44203" target="_blank"><i class="fab fa-linkedin-in fa-2x"></i>
+                            </a>
+                        </div>
+                        <div class="links-social">
+                            <a href="Not Found"><i class="fab fa-slack-hash fa-2x"></i></a>
+                        </div>
+                        <div class="links-social">
+                            <a href="Not Found"><i class="fab fa-twitter fa-2x"></i></a>
+                        </div>
+                    </div>
+                    <p class="keepInTouch">INFORMATION</p>
+                    <div class="links">
+                        <div class="left-links ">
+                            <a href="#" class="link-dark text-decoration-none">About Fapster app</a>
+                            <a href="#" class="link-dark text-decoration-none">Our team</a>
+                            <a href="#" class="link-dark text-decoration-none">Things We Like</a>
+                        </div>
+                        <div class="right-links">
+                            <a href="#" class="link-dark text-decoration-none">We Are Hiring</a>
+                            <a href="#" class="link-dark text-decoration-none">Privacy Policy</a>
+                            <a href="#" class="link-dark text-decoration-none">Terms of Service</a></div>
+                    </div>
+                </div>
+
+                <div class="footer-right-side">
+                    <p class="contact">CONTACT</p>
+                    <h1 class="contactUs">hello@fapsterapp.com</h1>
+                    <p class="keepInTouch">KEEP IN TOUCH</p>
+
+                    <form class="form-card">
+                        <div class="row  text-left">
+                            <div class="form-group col-sm-5 flex-column d-flex">
+                                <label class="form-control-label px-3"><span class="text-danger">
+                                </span>
+                                </label>
+                                <input type="text" id="fname" name="fname" placeholder="Your name" onblur="validate(1)">
+                            </div>
+                            <div class="form-group col-sm-5 flex-column d-flex">
+
+                                <label class="form-control-label px-3"><span class="text-danger">
+
+                            </span>
+                                </label>
+                                <input type="text" id="lname" name="lname" placeholder="E-mail" onblur="validate(2)">
+                            </div>
+                        </div>
+                        <div class="row justify-content-between">
+                            <div class="col-md-8">
+                                <label for="form_message"><span class="text-danger">
+                            </span>
+                                </label>
+                                <textarea id="form_message" name="message" class="form-control"
+                                          placeholder="Leave your message" rows="2" required="required"
+                                          data-error="Please, leave us a message." ></textarea>
+                            </div>
+                            <div class="form-group col-sm-4">
+                                <button type="submit" class="btn-block btn-primary">Send <i
+                                        class="fas fa-arrow-right"></i></button>
+                            </div>
+                        </div>
+
+                    </form>
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
