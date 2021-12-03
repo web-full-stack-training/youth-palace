931f98f48ca1c6b5b67570d4a28d82b896a19d95
Anjela <xachaturyananjela62@gmail.com>
2021-12-03 22:59:43 +0400

    create header

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,46 @@
+<link rel="stylesheet" href="{{ asset('css/header.css') }}">
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+
+<div class="first-part-header">
+    <h1 class="first">Follow Us</h1>
+    <div class="social">
+        <i class="fab fa-facebook facebook"></i>
+        <i class="fab fa-instagram insta"></i>
+        <i class="fab fa-twitter twitter"></i>
+    </div>
+
+    <div class="phone">
+        <i class="fal fa-phone-alt"></i>
+        <h3 class="phone-number">155785824</h3>
+    </div>
+    <div class="menu-adress">
+        <i class="fas fa-map-marker-alt location"></i>
+        <h3>83 Andy St. Madison NJ 78002</h3>
+    </div>
+    <div class="language">
+        <i class="far fa-globe"></i>
+        <select>
+            <option selected>English</option>
+            <option value="2">Руский</option>
+            <option value="3">Հայերեն</option>
+        </select>
+
+    </div>
+</div>
+<div class="second-header">
+    <img src="{{ asset('/photo/youthpalacelogo 1.png)') }}">
+    <h1 class="Youth">Youth </h1>
+    <div class="palace">
+        <h2>Palace</h2>
+    </div>
+
+    <div class="header-href">
+        <a href="#">Special Programs</a>
+        <a href="#">Clubs</a>
+        <a href="#">Collaboration</a>
+        <a href="#">Volunteering</a>
+        <a href="#">Media</a>
+        <a href="#">Feedback</a>
+        <a href="#">About as</a>
+    </div>
+</div>
