a9f6a435c414e9fd4ce9f65a9f4d5e6f70e632fa
anjela <xachaturyananjela62@gmail.com>
2021-12-09 21:16:27 +0400

    lengthen width my divs

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,37 +1,43 @@
 <link rel="stylesheet" href="{{ asset('css/header.css') }}">
 <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 
-<div class="first-part-header">
-    <h1 class="first">Follow Us</h1>
-    <div class="social">
-        <i class="fab fa-facebook facebook"></i>
-        <i class="fab fa-instagram insta"></i>
-        <i class="fab fa-twitter twitter"></i>
+<<div class="first-part-header">
+    <div>
+        <h1 class="first">Follow Us</h1>
+        <div class="social">
+            <i class="fab fa-facebook facebook"></i>
+            <i class="fab fa-instagram insta"></i>
+            <i class="fab fa-twitter twitter"></i>
+        </div>
     </div>
 
-    <div class="phone">
-        <i class="fal fa-phone-alt"></i>
-        <h3 class="phone-number">155785824</h3>
-    </div>
-    <div class="menu-adress">
-        <i class="fas fa-map-marker-alt location"></i>
-        <h3>83 Andy St. Madison NJ 78002</h3>
-    </div>
-    <div class="language">
-        <i class="far fa-globe"></i>
-        <select>
-            <option selected>English</option>
-            <option value="2">Руский</option>
-            <option value="3">Հայերեն</option>
-        </select>
+    <div>
+        <div class="phone">
+            <i class="fal fa-phone-alt"></i>
+            <h3 class="phone-number">155785824</h3>
+        </div>
+        <div class="menu-adress">
+            <i class="fas fa-map-marker-alt location"></i>
+            <h3>83 Andy St. Madison NJ 78002</h3>
+        </div>
+        <div class="language">
+            <i class="far fa-globe"></i>
+            <select>
+                <option selected>English</option>
+                <option value="2">Руский</option>
+                <option value="3">Հայերեն</option>
+            </select>
 
+        </div>
     </div>
+
 </div>
+
 <div class="second-header">
-    <img src="{{ asset('/photo/youthpalacelogo 1.png)') }}">
-    <h1 class="Youth">Youth </h1>
-    <div class="palace">
-        <h2>Palace</h2>
+    <div class="youth-palace">
+        <img src="{{ asset('/photo/youthpalacelogo 1.png)') }}">
+        <h1 class="Youth">Youth </h1>
+        <h1 class="palace">Palace</h1>
     </div>
 
     <div class="header-href">
