37f6b49715ca13dcc14d54677a81b3cffda66b12
samvel <samvhux@gmail.com>
2021-12-10 20:17:52 +0400

    create home page

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [\App\Http\Controllers\HomeController::class, 'index']);
+Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index']);
+
 
 
