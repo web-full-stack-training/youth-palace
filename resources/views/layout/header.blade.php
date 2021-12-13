8d8720e920e607143d7c5cd677d0ea9f011c3068
Sanasar Yuzbashyan <sanasar.yuzbashyan@gmail.com>
2021-12-13 21:04:22 +0400

    Fixed issues in header

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,46 +1,40 @@
 <link rel="stylesheet" href="{{ asset('css/header.css') }}">
 <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
-
-<<div class="first-part-header">
-    <div>
-        <h1 class="first">Follow Us</h1>
-        <div class="social">
-            <i class="fab fa-facebook facebook"></i>
-            <i class="fab fa-instagram insta"></i>
-            <i class="fab fa-twitter twitter"></i>
+<div class="header row d-flex align-items-center m-0">
+    <div class="left-side d-flex align-items-center col-md-4 col-xs-12">
+        <h4 class="text-white font-weight-bold m-0">Follow Us</h4>
+        <div class="social-medias">
+            <i class="fab fa-facebook"></i>
+            <i class="fab fa-instagram"></i>
+            <i class="fab fa-twitter"></i>
         </div>
     </div>
-
-    <div>
-        <div class="phone">
+    <div class="right-side d-flex align-items-center justify-content-end col-md-8 col-xs-12">
+        <div class="d-flex align-items-center px-3">
             <i class="fal fa-phone-alt"></i>
-            <h3 class="phone-number">155785824</h3>
+            <p class="text-white mb-0">155785824</p>
         </div>
-        <div class="menu-adress">
-            <i class="fas fa-map-marker-alt location"></i>
-            <h3>83 Andy St. Madison NJ 78002</h3>
+        <div class="d-flex align-items-center px-3">
+            <i class="fas fa-map-marker-alt"></i>
+            <p class="text-white font-weight-bold mb-0">83 Andy St. Madison NJ 78002</p>
         </div>
-        <div class="language">
+        <div class="d-flex align-items-center px-1">
             <i class="far fa-globe"></i>
             <select>
                 <option selected>English</option>
                 <option value="2">Руский</option>
                 <option value="3">Հայերեն</option>
             </select>
-
         </div>
     </div>
-
 </div>
-
-<div class="second-header">
-    <div class="youth-palace">
-        <img src="{{ asset('/photo/youthpalacelogo 1.png)') }}">
-        <h1 class="Youth">Youth </h1>
-        <h1 class="palace">Palace</h1>
+<div class="sub-header">
+    <div class="site-logo">
+        <img src="{{ asset('/images/icons/logo.svg') }}" alt="Logo" width="58" height="58">
+        <p>Youth </p>
+        <p>Palace</p>
     </div>
-
-    <div class="header-href">
+    <div class="header-menus">
         <a href="#">Special Programs</a>
         <a href="#">Clubs</a>
         <a href="#">Collaboration</a>
