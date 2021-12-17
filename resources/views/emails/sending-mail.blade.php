4f96c32315ff56abdb753ecb9119df1ca0b3845b
Sanasar Yuzbashyan <sanasar.yuzbashyan@gmail.com>
2021-12-17 21:13:21 +0400

    Fixed issue with send email

--- resources/views/emails/sending-mail.blade.php
+++ resources/views/emails/sending-mail.blade.php
@@ -1,13 +1,82 @@
-@component('emails::message')
-# Introduction
 
-The body of your message.
+<!doctype html>
+<html lang="en-US">
 
-@component('emails::button', ['url' => '999@gmail.com'])
-Button Text
-@endcomponent
+<head>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+    <title>New Account Email Template</title>
+    <meta name="description" content="New Account Email Template.">
+    <style type="text/css">
+        a:hover {text-decoration: underline !important;}
+    </style>
+</head>
 
-<div>
-    Name: {{ $user }}
-</div>
-@endcomponent
+<body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
+<!-- 100% body table -->
+<table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
+       style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
+    <tr>
+        <td>
+            <table style="background-color: #f2f3f8; max-width:670px; margin:0 auto;" width="100%" border="0"
+                   align="center" cellpadding="0" cellspacing="0">
+                <tr>
+                    <td style="height:80px;">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td style="text-align:center;">
+                        <a href="https://rakeshmandal.com" title="logo" target="_blank">
+                            <img width="60" src="{{ url('images/icons/logo.svg') }}" title="logo" alt="logo">
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="height:20px;">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>
+                        <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
+                               style="max-width:670px; background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
+                            <tr>
+                                <td style="height:40px;">&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td style="padding:0 35px;">
+                                    <h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">
+                                        Welcome!
+                                    </h1>
+                                    <p style="font-size:15px; color:#455056; margin:8px 0 0; line-height:24px;">
+                                        Hi <strong>{{ $user }},</strong> thank you for your interest in volunteering with us.</p>
+                                    <span
+                                        style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
+                                    <p
+                                        style="color:#455056; font-size:18px;line-height:20px; margin:0; font-weight: 500;">
+                                        We received your email and will answer it in order it was received.
+                                    </p>
+                                    <p style="font-style: italic; color: grey">No reply needed</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="height:40px;">&nbsp;</td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="height:20px;">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td style="text-align:center;">
+                        <p style="font-size:14px; color:rgba(69, 80, 86, 0.7411764705882353); line-height:18px; margin:0 0 0;">&copy; <strong>{{ env('APP_URL') }}</strong> </p>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="height:80px;">&nbsp;</td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
+<!--/100% body table-->
+</body>
+
+</html>
