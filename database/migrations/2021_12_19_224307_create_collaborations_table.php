ec68c91739b9216651f3e52ac63e7773931b871c
samvel <samvhux@gmail.com>
2021-12-21 22:15:55 +0400

    create tables

---
+++ database/migrations/2021_12_19_224307_create_collaborations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCollaborationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('collaborations', function (Blueprint $table) {
+            $table->id()->autoIncrement()->primary();
+            $table->text('img_path');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('collaborations');
+    }
+}
