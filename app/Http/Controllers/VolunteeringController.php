691fed38fae0e0886502f487df95247b65f31b1a
“narine-hovhannisyan” <narine2233@gmail.com>
2021-12-23 23:54:32 +0400

    volunteering page

--- app/Http/Controllers/VolunteeringController.php
+++ app/Http/Controllers/VolunteeringController.php
@@ -17,24 +17,19 @@ class VolunteeringController extends Controller
         $request->validate([
             'full_name' => 'required',
         ]);
-
-        // ensure the request has a file before we attempt anything else.
         if ($request->hasFile('image')) {
 
             $request->validate([
-                'image' => 'mimes:jpeg,bmp,png' // Only allow .jpg, .bmp and .png file types.
+                'image' => 'mimes:jpeg,bmp,png'
             ]);
-
-            // Save the file locally in the storage/public/ folder under a new folder named /product
             $request->image->store('volunteering', 'public');
-            // Store the record, using the new file hashname which will be it's new filename identity.
             $volunteering = new Volunteering([
                 "full_name" => $request->get('full_name'),
                 "image_path" => $request->image->hashName()
             ]);
-            $volunteering->save(); // Finally, save the record.
+            $volunteering->save();
         }
-/*        return view('volunteerings.create');*/
+        return view('volunteering.volunteering');
 
     }
 }
