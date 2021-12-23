1ce42dd382bf3ed5cd237620ff2e4f72ebeb44d3
samvel <samvhux@gmail.com>
2021-12-23 17:04:05 +0400

    create 2 tables and update code

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,13 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Models\About;
+use App\Models\LatestNews;
+use App\Models\OurWork;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home.home');
+        $ourWork = OurWork::all();
+//        $latestNews = new LatestNews();
+//        $latestNews->img_path = 'images/volunteers-needed.jpg';
+//        $latestNews->title = 'Made by reusable components';
+//        $latestNews->description = 'Let me explain the meaning reusable component as I feel it contextually';
+//        $latestNews->save();
+        $latestNews = LatestNews::all();
+        return view('home.home', compact('ourWork', 'latestNews'));
     }
 }
 
