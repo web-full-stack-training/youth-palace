381d92986e48cebc2d5eb10402f8f48209b92eeb
nelsonpetikyan95 <nelsgyumri@gmal.com>
2021-12-28 01:12:45 +0400

    Create-clubs-page

--- resources/views/clubs/clubs.blade.php
+++ resources/views/clubs/clubs.blade.php
@@ -1,16 +1,16 @@
 @extends('app')
 @section('content')
          <link rel="stylesheet" href="{{ asset('css/clubs.css') }}">
-
-         <div class = 'row clubs-container justify-content-center'>
+         <!--<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">-->
+         <div class = 'row clubs-container '>
 
                  <div class = 'title-clubs'>
-                         <h1 class = 'title-page d-flex justify-content-center'>Clubs</h1>
+                         <h1 class = 'title-page text-center'>Clubs</h1>
                      </div>
-                 <div class = 'card-container row mt-44 p-0'>
-                     <div class = 'col-6 first-card-content'>
+                 <div class = 'row card-container  mt-44 p-0'>
+                     <div class = ' col-6 first-card-content'>
                            <div class = 'first-card-image-content'>
-                                 <img src='images/clubs/' width = "370px" height = "250px" alt="first-image.jpeg">
+                                 <img src='images/clubs/first_image.jpeg' width = '370px' height = '250px' alt="first image">
                                </div>
                            <div class = 'first-card-description-content'>
                                  <h1 class = 'first-card-title'> REUSABLE COMPONENTS</h1>
@@ -20,8 +20,8 @@
                                </div>
                          </div>
                      <div class = 'col-6 second-card-content'>
-                           <div class = 'col-6 second-image-card-content '>
-                                 <img src = 'images/clubs/second-image.jpeg' width = "370px" height = "250px">
+                           <div class = 'second-image-card-content'>
+                                 <img src = 'images/clubs/second-image.jpeg' width = "370px" height = "250px" alt="second image">
                                </div>
                            <div class = 'second-card-description-content'>
                                    <h1 class = 'second-card-title'>REUSABLE COMPONENTS</h1>
@@ -31,7 +31,7 @@
                                </div>
                          </div>
                          <div class = 'col-6 third-card-content'>
-                                <div class = 'col-6  third-image-card-content  !important'>
+                                <div class = 'third-image-card-content  !important'>
                                         <img src = 'images/clubs/third-image.jpeg' width = '370px' height = '250px'>
                                     </div>
                                  <div class = 'third-card-description-content'>
@@ -41,8 +41,8 @@
                                          when designing interfaces in Figma?</p>
                                      </div>
                          </div>
-                     <div class = 'col-6  fourth-card-content'>
-                             <div class = 'col-6  fourth-image-card-content'>
+                     <div class = 'col-6 fourth-card-content'>
+                             <div class = 'fourth-image-card-content'>
                                  <img src = 'images/clubs/fourth-image.jpeg' width = '370px' height = '250px'>
                                  </div>
                              <div class = 'fourth-card-description-content'>
@@ -52,8 +52,9 @@
                                          when designing interfaces in Figma?</p>
                                  </div>
                          </div>
+
                      <div class = 'col-6  five-card-content'>
-                             <div class = 'col-6  five-image-card-content'>
+                             <div class = 'col five-image-card-content'>
                                  <img src = 'images/clubs/fifth-image.jpeg' width = '370px' height = '250px'>
                                  </div>
                              <div class = 'five-card-description-content'>
@@ -65,7 +66,7 @@
                          </div>
 
                      <div class = 'col-6  six-card-content'>
-                             <div class = 'col-6  six-image-card-content'>
+                             <div class = ' six-image-card-content'>
                                  <img src = 'images/clubs/sixth-image.jpeg' width = '370px' height = '250px'>
                                  </div>
                              <div class = 'six-card-description-content'>
@@ -77,3 +78,4 @@
                          </div>
                  </div>
             </div>
+@endsection
